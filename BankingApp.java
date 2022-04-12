9b3e409e5d511cf81f4a709db63aaf7f8b7150e4
Shaira Asis <shairaasis99@gmail.com>
2022-04-12 16:01:19 +0800

    Added comments

--- BankingApp.java
+++ BankingApp.java
@@ -10,41 +10,45 @@ public class BankingApp {
     public static void main(String arg[]) {  
         String currentCurrency = currency[0];
         Scanner sc = new Scanner(System.in);
-        //Initial ask user to input name
-        for(int index=0; index<accounts.length; index++){
-            System.out.println("Enter your name: ");  
-            accounts[index]=sc.nextLine();    
-            userNumber [0] = 1;
-            balance[0] = 10000;
-        }
-
-
-
+        //2. Initially ask the user to input his Name and set the initial funds to 10,000 and the initial currency to PHP.
+        System.out.println("Enter your name: ");  
+        accounts[0]=sc.nextLine();    
+        userNumber [0] = 1;
+        balance[0] = 10000;
+            
+        
         int choice;
+        //Program will keep on running until the user inputs 0. Exit 
         do{
             
             System.out.println("\n\n ***Choose Transaction***"); 
             System.out.println("----------------------------"); 
             System.out.println("0.Exit\n1.Display Information\n2.Add Funds\n3.Withdraw funds\n4.Convert Funds");  
             System.out.println("----------------------------"); 
+            
+            //Ask the user to input choice of transaction (store the input to choice)
             System.out.println("Enter choice: ");
             choice = sc.nextInt();
             
 
             switch(choice){
                 case 0: 
+                    //Program ends / Exit
                     System.out.println("\nThank you for using my Banking App!");
                 break;
 
                 case 1:
+                    //Call the method for Displaying Account Information
                     Methods.DisplayInformation(currentCurrency);
                 break;
 
                 case 2:
+                    //Call the method for Adding Funds to Account
                     Methods.AddFunds(currentCurrency);
                 break;
 
                 case 3:
+                    //Call the method for Withdrawing Funds from Account
                     Methods.WithdrawFunds(currentCurrency);
                 break;
 
@@ -57,62 +61,74 @@ public class BankingApp {
                     System.out.println("-------------------");
                     System.out.println("1."+BankingApp.currency[1] +"\n2." +BankingApp.currency[2]);
                     System.out.println("----------------------");
+                    //Ask the user to choose currency (store the input to currencyChosen)
                     System.out.println("Choose Currency:");
                     int currencyChosen = sc.nextInt();
                     System.out.println("-------------------");
                         if(currencyChosen==1){
-
+                            //Call the Method for Converting Funds From PHP to USD
                             Methods.ConvertFundsFromPhpToUsd(currentCurrency);
+                            //The method will return currentcCurrency from Methods Class (Now USD) and assign it to currentCurrency of BankingApp class
                             currentCurrency = BankingApp.currentCurrency;
                         }
                         
                         else if(currencyChosen ==2){
+                            //Call the Method for Converting Funds From PHP to JPY
                             Methods.ConvertFundsFromPhpToJPY(currentCurrency);
+                            //The method will return currentcCurrency from Methods Class (Now JPY)and assign it to currentCurrency of BankingApp class
                             currentCurrency = BankingApp.currentCurrency;
                         }
 
                     }
 
-
+                    //If current currency is USD, only PHP and JPY are available to convert to
                     else if(currentCurrency == "USD"){
                     System.out.println("Available Currencies");
                     System.out.println("-------------------");
                     System.out.println("1."+BankingApp.currency[0] +"\n2." +BankingApp.currency[2]);
                     System.out.println("----------------------");
+                    //Ask the user to choose currency (store the input to currencyChosen)
                     System.out.println("Choose Currency:");                   
                     int currencyChosen = sc.nextInt();
                     System.out.println("-------------------");
                         if(currencyChosen==1){
-
+                            //Call the Method for Converting Funds From USD to PHP
                             Methods.ConvertFundsFromUsdToPhp(currentCurrency);
+                            //The method will return currentCurrency from Methods Class (Now PHP) and assign it to currentCurrency of BankingApp class
                             currentCurrency = BankingApp.currentCurrency;
                         }
                         
                         else if(currencyChosen ==2){
+                            //Call the Method for Converting Funds From USD to JPY
                             Methods.convertFundsFromUsdToJpy(currentCurrency);
+                            //The method will return currentCurrency from Methods Class (Now JPY) and assign it to currentCurrency of BankingApp class
                             currentCurrency = BankingApp.currentCurrency;
                         }
 
                     }
 
 
-
+                    //If current currency is JPY, only PHP and USD are available to convert to
                     else if(currentCurrency == "JPY"){
                     System.out.println("Available Currencies");
                     System.out.println("-------------------");
                     System.out.println("1."+BankingApp.currency[0] +"\n2." +BankingApp.currency[1]);
                     System.out.println("----------------------");
+                    //Ask the user to choose currency (store the input to currencyChosen)
                     System.out.println("Choose Currency:");
                     int currencyChosen = sc.nextInt();
                     System.out.println("-------------------");
                         if(currencyChosen==1){
-
+                            //Call the Method for Converting Funds From JPY to PHP
                             Methods.convertFundsFromJpyToPhp(currentCurrency);
+                            //The method will return currentCurrency from Methods Class (Now PHP) and assign it to currentCurrency of BankingApp class
                             currentCurrency = BankingApp.currentCurrency;
                         }
                         
                         else if(currencyChosen ==2){
+                            //Call the Method for Converting Funds From JPY to USD
                             Methods.convertFundsFromJpyToUsd(currentCurrency);
+                            //The method will return currentCurrency from Methods Class (Now USD) and assign it to currentCurrency of BankingApp class
                             currentCurrency = BankingApp.currentCurrency;
                         }
     
--- Methods.java
+++ Methods.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Methods {
     static String newCurrency;
 
+    //Method for Display Account Information
     public static void DisplayInformation(String currentCurrency) {
         System.out.println("\n ***Account Information***"); 
         System.out.println("----------------------------"); 
@@ -11,11 +12,13 @@ public class Methods {
         
     }
 
+    //Method for Adding Funds to the Account
     public static void AddFunds(String currentCurrency) { 
         Scanner sc = new Scanner(System.in);
         System.out.println("\n ***Add Funds***"); 
         System.out.println("----------------------------"); 
         for (int count=0; count<BankingApp.accounts.length; count++){
+        //Ask user to input amount to be added to the account and store the value to amount
         System.out.println("Enter the amount you want to add: ");  
         double amount = sc.nextDouble();  
             if(amount<500){
@@ -24,6 +27,7 @@ public class Methods {
             }
 
             else {
+                //Add the amount entered to the balance and assign the result of balance
                 BankingApp.balance[count] = BankingApp.balance[count] + amount;  
                 System.out.println("You successfully added "+currentCurrency +amount +" to your account.");
             }
@@ -39,9 +43,11 @@ public class Methods {
         System.out.println("\n ***Withdraw Funds***"); 
         System.out.println("----------------------------"); 
         for (int count=0; count < BankingApp.accounts.length; count++){
-            System.out.println("Enter the amount you want to withdraw: ");  
-            double amount = sc.nextDouble();
+        //Ask user to input amount to be withdrawn from the account and store the value to amount
+        System.out.println("Enter the amount you want to withdraw: ");  
+        double amount = sc.nextDouble();
             if(amount<=BankingApp.balance[count] && amount >= 500){
+                //Subtract the amount entered from the balance and assign the result to balance
                 BankingApp.balance[count] = BankingApp.balance[count] - amount;  
                 System.out.println("You have successfully withdrawn "+currentCurrency +amount +" from your account.");
             }
@@ -65,15 +71,18 @@ public class Methods {
     //Method to convert FROM PHP TO USD
     public static String ConvertFundsFromPhpToUsd(String currentCurrency) {
         for (int count=0; count<BankingApp.accounts.length; count++){
+            //Assign currentCurrency to currency[1] which is "USD"
             BankingApp.currentCurrency=BankingApp.currency[1];
             currentCurrency = BankingApp.currency[1];
+            //Display information of the conversion
             System.out.println("1 PHP = 0.019185473 USD");
             System.out.println("PHP "+BankingApp.balance[count]+ " = USD "+(BankingApp.balance[count]*0.019185473));
+            //Calculate the conversion by multiplying balance to the conversion rate and assign the result to balance
             BankingApp.balance[count] = BankingApp.balance[count] * 0.019185473;
             System.out.println("Conversion Successful.");
             //System.out.println("Name: " + BankingApp.accounts[count]+ " Funds: " + BankingApp.balance[count] +"Current Currency:" +currentCurrency);
         }
-
+        //return currentCurrency to main which is USD
         return currentCurrency;
 
     }
@@ -82,100 +91,96 @@ public class Methods {
     //Method to convert FROM PHP TO JPY
     public static String ConvertFundsFromPhpToJPY(String currentCurrency) {
         for (int count=0; count<BankingApp.accounts.length; count++){
+            //Assign currentCurrency to currency[2] which is "JPY"
             BankingApp.currentCurrency=BankingApp.currency[2];
             currentCurrency = BankingApp.currency[2];
+            //Display information of the conversion
             System.out.println("1 PHP = 2.41 JPY");
-            System.out.println("PHP "+BankingApp.balance[count]+ " = USD" +(BankingApp.balance[count]*2.41));
+            System.out.println("PHP "+BankingApp.balance[count]+ " = JPY" +(BankingApp.balance[count]*2.41));
+            //Calculate the conversion by multiplying balance to the conversion rate and assign the result to balance
             BankingApp.balance[count] = BankingApp.balance[count] * 2.41;
             System.out.println("Conversion Successful.");
             //System.out.println("Name: " + BankingApp.accounts[count]+ " Funds: " + BankingApp.balance[count] +"Current Currency:" +currentCurrency);
         }
-
+        //return currentCurrency to main which is JPY
         return currentCurrency;
 
     }
-
-
-
-
-
-
 
 
     //Method to convert FROM USD TO PHP
     public static String ConvertFundsFromUsdToPhp(String currentCurrency) {
         for (int count=0; count<BankingApp.accounts.length; count++){
+            //Assign currentCurrency to currency[0] which is "PHP"
             BankingApp.currentCurrency=BankingApp.currency[0];
             currentCurrency = BankingApp.currency[0];
+            //Display information of the conversion
             System.out.println("1 USD = 52.12277 PHP");
             System.out.println("USD " + BankingApp.balance[count]+ " = PHP " +(BankingApp.balance[count]*52.12277));
+            //Calculate the conversion by multiplying balance to the conversion rate and assign the result to balance
             BankingApp.balance[count] = BankingApp.balance[count] * 52.12277;
             System.out.println("Conversion Successful.");
             //System.out.println("Name: " + BankingApp.accounts[count]+ " Funds: " + BankingApp.balance[count] +"Current Currency:" +currentCurrency);
         }
-
+        //return currentCurrency to main which is PHP
         return currentCurrency;
 
     }
+
     //Method to convert FROM USD TO JPY
     public static String convertFundsFromUsdToJpy(String currentCurrency) {
         for (int count=0; count<BankingApp.accounts.length; count++){
+            //Assign currentCurrency to currency[2] which is "JPY"
             BankingApp.currentCurrency=BankingApp.currency[2];
             currentCurrency = BankingApp.currency[2];
+            //Display information of the conversion
             System.out.println("1 USD = 125.40972 JPY");
             System.out.println("USD " +BankingApp.balance[count]+ " = JPY " +(BankingApp.balance[count]*125.40972));
+            //Calculate the conversion by multiplying balance to the conversion rate and assign the result to balance
             BankingApp.balance[count] = BankingApp.balance[count] * 125.40972;
             System.out.println("Conversion Successful.");
             //System.out.println("Name: " + BankingApp.accounts[count]+ " Funds: " + BankingApp.balance[count] +" Current Currency:" +currentCurrency);
         }
-
+        //return currentCurrency to main which is JPY
         return currentCurrency;
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     //Method to convert FROM JPY TO PHP
     public static String convertFundsFromJpyToPhp(String currentCurrency) {
         for (int count=0; count<BankingApp.accounts.length; count++){
+            //Assign currentCurrency to currency[0] which is "PHP"
             BankingApp.currentCurrency=BankingApp.currency[0];
             currentCurrency = BankingApp.currency[0];
+            //Display information of the conversion
             System.out.println("1 JPY = 0.4154494 PHP");
             System.out.println("JPY " + BankingApp.balance[count]+ " = PHP " +(BankingApp.balance[count]*0.4154494));
+            //Calculate the conversion by multiplying balance to the conversion rate and assign the result to balance
             BankingApp.balance[count] = BankingApp.balance[count] * 0.4154494;
             System.out.println("Conversion Successful.");
             //System.out.println("Name: " + BankingApp.accounts[count]+ " Funds: " + BankingApp.balance[count] +"Current Currency:" +currentCurrency);
         }
-
+        //return currentCurrency to main which is PHP
         return currentCurrency;
 
     }
+
     //Method to convert FROM JPY TO USD
     public static String convertFundsFromJpyToUsd(String currentCurrency) {
         for (int count=0; count<BankingApp.accounts.length; count++){
+            //Assign currentCurrency to currency[1] which is "USD"
             BankingApp.currentCurrency=BankingApp.currency[1];
             currentCurrency = BankingApp.currency[1];
+            //Display information of the conversion
             System.out.println("1 JPY = 0.0079740937 USD");
             System.out.println("JPY " + BankingApp.balance[count]+ " = USD " +(BankingApp.balance[count]*0.0079740937));
+            //Calculate the conversion by multiplying balance to the conversion rate and assign the result to balance
             BankingApp.balance[count] = BankingApp.balance[count] * 0.0079740937;
             System.out.println("Conversion Successful.");
             //System.out.println("Name: " + BankingApp.accounts[count]+ " Funds: " + BankingApp.balance[count] +" Current Currency:" +currentCurrency);
         }
-
+        //return currentCurrency to main which is USD
         return currentCurrency;
 
     }
